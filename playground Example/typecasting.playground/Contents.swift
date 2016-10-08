18e6ba04ceb2f415a9eecc8fce6232bb12c092b3
BoshiLee <boshi.litw@gmail.com>
2016-10-09 01:26:30 +0800

    update to L5

--- playground Example/typecasting.playground/Contents.swift
+++ playground Example/typecasting.playground/Contents.swift
@@ -8,13 +8,56 @@ class MediaItem {
         self.name = name
     }
 }
-
-class Music:MediaItem{
-    var artist:String?
-    init(name: String, artist:String)
-    {
+class Movie: MediaItem {
+    var director: String
+    init(name: String, director: String) {
+        self.director = director
+        super.init(name: name)
+    }
+}
+class Song: MediaItem {
+    var artist: String
+    init(name: String, artist: String) {
         self.artist = artist
         super.init(name: name)
     }
 }
-Music(name: "Song 2", artist: "Blur")
+
+var GoogleLibary =
+[
+    Movie(name: "Casablanca", director: "Michael Curtiz"),
+    Song(name: "Blue Suede Shoes", artist: "Elvis Presley"),
+    Movie(name: "Citizen Kane", director: "Orson Welles"),
+    Song(name: "The One And Only", artist: "Chesney Hawkes"),
+    Song(name: "Never Gonna Give You Up", artist: "Rick Astley"),
+    Song(name: "Song 2", artist: "Blur")
+]
+
+func castChecker(mediaLibray Libary: [MediaItem]) -> (songs: Int, movies: Int) {
+    var songCounter = 0
+    var moviesCounter = 0
+    for element in Libary{
+        if element is Song{
+            songCounter += 1
+        } else{
+            moviesCounter += 1
+        }
+    }
+    return (songCounter, moviesCounter)
+}
+let googlePlay = castChecker(mediaLibray: GoogleLibary)
+
+print(googlePlay.movies, googlePlay.songs)
+
+
+var things = [Any]()
+
+things.append(0)
+things.append(0.0)
+things.append(42)
+things.append(3.14159)
+things.append("hello")
+things.append((3.0, 5.0))
+things.append(Movie(name: "Ghostbusters", director: "Ivan Reitman"))
+things.append({ (name: String) -> String in "Hello, \(name)" })
+
